3d9bee593d7fe8c0701ef06f442521a44a71f7b1
Neo Neo <jesusggxbk@gmail.com>
2026-07-13 11:48:46 -0600

    Actualizar CORS para Vercel

--- src/main/java/com/flashcart/flashcart_backend/config/SecurityConfig.java
+++ src/main/java/com/flashcart/flashcart_backend/config/SecurityConfig.java
@@ -60,7 +60,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://flashcart-frontend-two.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
